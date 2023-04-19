7dabb9bb81648cc7af9df5afdd0b825e91893a9d
Seroblaye <sblancoyebes@educa.madrid.org>
2023-04-19 11:35:26 +0200

    Contenido del Tabla.vue actualizado para trabajar con Tabulator

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -1,43 +1,20 @@
 <template>
   <div>
 
-    <label for="busqueda">Introduce un número para filtrar: </label>
-    <input id="busqueda" v-model="searchText"  placeholder="Buscar...">
-    <br>
-
-    <vuetable ref="vuetable"
-    v-if="filteredData.length > 0"
-      :data="filteredData"
-      :fields="fields"
-      :api-mode="false"
-    ></vuetable>
-    
-    <div id="resultado" v-else>
-      <br>
-      No hay datos que incluyan la busqueda
-    </div>
-
-    <br>
-    <button @click="descargarCSV()">
-        Descargar CSV
-    </button>
-
   </div>
   </template>
   
   <script>
-  import Vuetable from 'vuetable-2'
-  import exportFromJSON from "export-from-json"
   
   export default {
     name: 'TablaAfluencia',
     components: {
-      Vuetable
+
     },
     data() {
       return {
         fields: ['id', 'afluencia', 'comparacion' ],
-        serch:'',
+
         data: {
           "data": [
             {
@@ -91,56 +68,8 @@
               "comparacion": 1000
             }
           ]
-        },
-        searchText: '',
-      }
-    }, 
-    
-    computed: {
-        filteredData() {
-          const busqueda = this.searchText;
-            return this.data.data.filter(obj => {
-              return obj.afluencia.toString() === busqueda  || obj.id.toString() === busqueda || obj.comparacion.toString().includes(busqueda);
-            });
-          },
-        },
-
-      methods: {
-        ordenarAfluencia() {
-            this.data.data.sort((a, b) => a.afluencia - b.afluencia);
-        },
-
-        operacionComparacion() {
-            this.data.data = this.data.data.map(obj => {
-              const operacion = ((obj.comparacion * 100) / obj.afluencia - 100).toFixed(2)
-              const color = operacion < 0 ? 'red' : 'green'
-              const comparacion = `${obj.comparacion} | <span style="color:${color}">${operacion}%</span>`
-              return {
-                ...obj,
-                comparacion
-              }
-          });
-        },
-
-        descargarCSV(){
-          const data = this.data.data.map(obj => {
-                return {
-                  ...obj,
-                  comparacion: obj.comparacion.replace(/<[^>]*>/g, '')
-                }
-              });
-
-          const nombreArchivo = 'Trabajo';
-          const exportType = exportFromJSON.types.csv;
-          exportFromJSON({data, fileName: nombreArchivo, exportType});
-          
         }
-      },
-
-      created() {
-        this.ordenarAfluencia();
-        this.operacionComparacion();
-        
-    }
+      }
+    } 
   }
 </script>
