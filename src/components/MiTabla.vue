7d3dd31c63cbd67193d2eb42b825a3c157320694
Seroblaye <sblancoyebes@educa.madrid.org>
2023-04-25 12:52:58 +0200

    Pequeñas modificaciones en ordenar por columnas y por default el afluencia

--- src/components/MiTabla.vue
+++ src/components/MiTabla.vue
@@ -25,16 +25,16 @@
     data() {
       return {
         data: [
-          {"id": 1, "afluencia": 1000, "comparacion": 1322 }, 
+          {"id": 1, "afluencia": 1000, "comparacion": 50 }, 
           {"id": 2, "afluencia": 500, "comparacion": 25 },
           {"id": 3, "afluencia": 750, "comparacion": 35},
-          {"id": 4, "afluencia": 1200, "comparacion": 16516},
+          {"id": 4, "afluencia": 1200, "comparacion": 60},
           {"id": 5, "afluencia": 900, "comparacion": 45},
           {"id": 6, "afluencia": 1500, "comparacion": 60 },
           {"id": 7, "afluencia": 800, "comparacion": 40},
-          {"id": 8, "afluencia": 1100, "comparacion": 51555},
+          {"id": 8, "afluencia": 1100, "comparacion": 55},
           {"id": 9, "afluencia": 650, "comparacion": 30},
-          {"id": 10, "afluencia": 950, "comparacion": 2135}
+          {"id": 10, "afluencia": 950, "comparacion": 0}
         ],
         searchText: ''
       }
@@ -55,21 +55,20 @@
     mounted() {
       this.table = new Tabulator("#mitabla", {
         pagination:true, 
-        paginationSize:3, 
+        paginationSize:5,
         columns: [
           { title: "ID", field: "id",sortable: true},
-          { title: "Afluencia", field: "afluencia",sortable: true},
-          { title: "Comparacion", field: "comparacion",formatter: this.operacionComparacion, sortable: true} //Uso el formater con una principal razon de uso pero sirve para dos cosas.
+          { title: "Afluencia", field: "afluencia", sortable: true, sorter:"number"},
+          { title: "Comparacion", field: "comparacion", formatter: this.operacionComparacion} //Uso el formater con una principal razon de uso pero sirve para dos cosas.
         ],
-        data: this.filteredData
-        
+        data: this.filteredData,
+        initialSort:[
+          {column:"afluencia", dir:"desc"}
+        ]
       });
     },
 
     methods: {
-      ordenarAfluencia() {
-        this.data.sort((a, b) => a.afluencia - b.afluencia);
-      },
       
       operacionComparacion(obj) {
           const operacion = ((obj.getValue() * 100) / obj.getRow().getData().afluencia - 100).toFixed(2); //La primera, me permite obtener el valor de cada fila de comparacion sin necesidad de hacerlo como en afluencia
@@ -84,16 +83,15 @@
           return {
             id: obj.id,
             afluencia: obj.afluencia,
-            comparacion: obj.comparacion//Deberia de pillar el comparacion del metodo anterior, pero no pilla la comparacion y la operacion, solo pilla el valor de comparacion
+            comparacion: obj.comparacion
           }
         });
-        this.table.download("csv","Trabajo.csv", datosDescarga,  {bom:true});
+        this.table.download("csv","Trabajo.csv", datosDescarga);
       }
       
     },
 
     created() {
-      this.ordenarAfluencia();
       this.operacionComparacion();
     },
 
