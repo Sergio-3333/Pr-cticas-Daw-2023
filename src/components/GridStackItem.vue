84a85e0df992645a3828aaf3dcdcfc6206ec7449
Seroblaye <sblancoyebes@educa.madrid.org>
2023-05-11 13:28:02 +0200

    Pequeño error de consola solucionado. Partimos de aqui

--- src/components/GridStackItem.vue
+++ src/components/GridStackItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :data-gs-id="id" class="grid-stack-item" gs-max-w="10" gs-h="10" gs-w="4" >
+  <div class="grid-stack-item" gs-max-w="10" gs-h="10" gs-w="4" >
     <div class="grid-stack-item-content">
     <slot></slot>
     </div>
